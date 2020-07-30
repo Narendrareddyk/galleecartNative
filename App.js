7287f0119603dde0c61ad472ec56cd83e788d122
Narendrareddyk <59686326+Narendrareddyk@users.noreply.github.com>
2020-07-30 16:40:26 +0530

    Add files via upload
    first upload files for create new project is gallecart

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+ 
+ function App() {
+   return (
+     <View style={{flex:1,}}>
+       <Text>App.js</Text>
+     </View>
+   )
+ }
+ 
+ export default App
+ 
